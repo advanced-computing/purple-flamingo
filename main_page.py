8fb474c9d40ebe313413305e7b83b2c75f6f58cc
Aria Kovalovich <aak2257@columbia.edu>
2026-03-12 13:33:56 -0400

    Add page for project proposals in navigation
    Added a new page for project proposals to the navigation.

--- main_page.py
+++ main_page.py
@@ -3,9 +3,10 @@ import streamlit as st
 # Define the pages
 main_page = st.Page("app.py", title="EIA Fuel Type Demand")
 page_2 = st.Page("region.py", title="EIA Region Demand")
-
+page_3 = st.Page("proposal.py", title="Project Proposal, Feedback, and Reflections")
 # Set up navigation
 pg = st.navigation([main_page, page_2])
 
 # Run the selected page
 pg.run()
+
